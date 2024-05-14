6e93217a1fccd118a40a8eb983d7c32d0898c03a
Ruben Sa <rubenjms1@gmail.com>
2024-05-13 22:31:51 -0300

    adicionando metodo update ao controler

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,6 +1,6 @@
 class ClientesController < ApplicationController
     def index
-       @clientes = Cliente.all 
+        @clientes = Cliente.order(:id)
     end
 
     def show
@@ -10,6 +10,13 @@ class ClientesController < ApplicationController
     def create
         @cliente = Cliente.new(clientes_params)
         return render "show", status: 201 if @cliente.save
+            
+        render json: @cliente.errors, status: 400     
+    end
+
+    def update
+        @cliente = Cliente.find(params[:id])
+        return render "show", status: 200 if @cliente.update(clientes_params)
             
         render json: @cliente.errors, status: 400     
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "/clientes/:id", to: "clientes#show"
   post "/clientes", to: "clientes#create"
   delete "/clientes/:id", to: "clientes#destroy"
+  patch "/clientes/:id", to: "clientes#update"
 end
